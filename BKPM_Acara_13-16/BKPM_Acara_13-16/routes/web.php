5986a6c715d00ceb92c23a3303cbbe88182f6fed
micheliacampaka <micheliacmpa@gmail.com>
2025-03-13 09:30:28 +0700

    BKPM_Acara_13-16

--- BKPM_Acara_13-16/BKPM_Acara_13-16/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\DashboardController;
-use Illuminate\Support\Facades\Route;
-
-Route::group(['namespace'=>'Backend'], function()
-{
-    Route::resource('dashboard' ,'DashboardController');
-});
